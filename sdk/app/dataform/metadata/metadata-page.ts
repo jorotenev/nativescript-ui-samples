5f93240cd6ebfd0d052c709d698761178ec08038
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-27 20:29:25 +0200

    showcase issue with the raddataform when using the validateAndCommitAll()

--- sdk/app/dataform/metadata/metadata-page.ts
+++ sdk/app/dataform/metadata/metadata-page.ts
@@ -1,5 +1,27 @@
 import viewModel = require("./../view-models/person-model-2");
+import {RadDataForm} from "nativescript-ui-dataform";
+
+
+let dataform: RadDataForm;
+let model: viewModel.PersonModel2;
+
 export function onPageLoaded(args) {
+
     var page = args.object;
-    page.bindingContext = new viewModel.PersonModel2();
+    dataform = page.getViewById("myDataForm")
+    model = new viewModel.PersonModel2()
+    page.bindingContext = model;
+}
+
+
+export function tapped() {
+    let age = dataform.getPropertyByName("age");
+    console.log(`valueCandidate=${age.valueCandidate}\nvalue=${age.value}`)
+    dataform.validateAndCommitAll().then((ok) => {
+        if (ok) {
+            console.log("ok");
+            console.dir(model.get('person'))
+            console.dir(JSON.parse(dataform.editedObject))
+        }
+    })
 }
