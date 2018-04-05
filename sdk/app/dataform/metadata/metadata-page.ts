3b834d05dc1b8606a3fa90b5f063401be8aeabc8
Georgi Tenev <georgetenevbg@gmail.com>
2018-04-05 13:54:03 +0300

    add comments

--- sdk/app/dataform/metadata/metadata-page.ts
+++ sdk/app/dataform/metadata/metadata-page.ts
@@ -8,20 +8,30 @@ let model: viewModel.PersonModel2;
 export function onPageLoaded(args) {
 
     var page = args.object;
-    dataform = page.getViewById("myDataForm")
-    model = new viewModel.PersonModel2()
+    dataform = page.getViewById("myDataForm");
+    model = new viewModel.PersonModel2();
     page.bindingContext = model;
 }
 
 
 export function tapped() {
+    // the form field we care about. it's defined as a "Number" in the metadata
     let age = dataform.getPropertyByName("age");
-    console.log(`valueCandidate=${age.valueCandidate}\nvalue=${age.value}`)
+
+    // the valueCandidate is whatever the user has entered (if any)
+    console.log(`valueCandidate=${age.valueCandidate}`);
+
+    // capture the user input for `age` here and fix it, because after validateAndCommitAll() it will be wiped out.
+    let fixedValue = Number(String(age.valueCandidate));
+
     dataform.validateAndCommitAll().then((ok) => {
         if (ok) {
-            console.log("ok");
-            console.dir(model.get('person'))
-            console.dir(JSON.parse(dataform.editedObject))
+            // `person.age` will be a seemingly empty object.
+            let person = model.get('person');
+            console.dir(person);
+            console.log("fixed value: " + fixedValue)
+        } else {
+            console.log("validation failed")
         }
     })
 }
